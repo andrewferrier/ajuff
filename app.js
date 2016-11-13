cf3ca8cf1ad8eb2713614a27efe1859358fc2215
Andrew Ferrier <andrewferrier@example.com>
2016-11-13 20:37:38 +0000

    Initial use of alexa-app-server.

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
 /*eslint-env node*/
 
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
-
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-var express = require('express');
-
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
-// create a new express server
-var app = express();
-
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
-
-// get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+var AlexaAppServer = require('alexa-app-server');
+AlexaAppServer.start({
+    server_root: __dirname,
+    public_html: "public",
+    app_dir: "apps",
+    app_root: "/alexa/",
+    port: appEnv.port,
+    post: (appServer) => {
+        console.log("server starting on " + appEnv.url);
+    }
 });
