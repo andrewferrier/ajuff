30b049381fab7293d1c0ead08bef029963576e1f
Andrew Ferrier <andrewferrier@example.com>
2016-11-13 20:51:45 +0000

    Add brunel app.

---
+++ apps/brunel.js
@@ -0,0 +1,16 @@
+var alexa = require('alexa-app');
+
+// Allow this module to be reloaded by hotswap when changed
+module.change_code = 1;
+
+var app = new alexa.app('Brunel');
+
+app.launch(function(req, res) {
+    res.say("Hello from Brunel");
+});
+
+app.intent('GetTime', {}, function(req, res) {
+    res.say('The time is unpredictable');
+});
+
+module.exports = app;
